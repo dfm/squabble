369e836cd54f37dbe37bc5f31ac38aaa102b7e4e
Dan F-M <foreman.mackey@gmail.com>
2020-11-29 10:06:27 -0500

    cleanup

--- src/squabble/__init__.py
+++
@@ -1,7 +0,0 @@
-# __all__ = []
-
-__uri__ = "https://github.com/dfm/squabble"
-__author__ = "Daniel Foreman-Mackey"
-__email__ = "foreman.mackey@gmail.com"
-__license__ = "MIT"
-__description__ = ""
--- src/squabble/sql/tables.sql
+++
@@ -1,8 +0,0 @@
--- create users table
-CREATE TABLE IF NOT EXISTS users
-(
-  id integer PRIMARY KEY,
-  username character varying(256) UNIQUE,
-  password character varying(256) NOT NULL,
-  disabled boolean NOT NULL DEFAULT false
-);
